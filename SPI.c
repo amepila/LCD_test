626b88ada479f065e0dd1ff17bfb9311b1094bd8
amepila <ie704453@iteso.mx>
2017-10-06 13:04:31 -0500

    First commit

---
+++ SPI.c
@@ -0,0 +1,213 @@
+/*
+ * SPI.c
+ *
+ *  Created on: Sep 27, 2017
+ *      Author: Andres Hernandez
+ */
+
+#include "SPI.h"
+
+/*it enable the clock module of the SPI by modifying the MDIS bits*/
+static void SPI_enable(SPI_ChannelType channel){
+	switch(channel){
+	case SPI_0:
+		SPI0->MCR |= 0<<BIT14;
+		break;
+	case SPI_1:
+		SPI1->MCR |= 0<<BIT14;
+		break;
+	case SPI_2:
+		SPI2->MCR |= 0<<BIT14;
+		break;
+	default:
+		break;
+	}
+}
+/*It activate the clock gating*/
+static void SPI_clk(SPI_ChannelType channel){
+	switch(channel){
+	case SPI_0:
+		SIM->SCGC6 |= SPI0_CLOCK_GATING;
+		break;
+	case SPI_1:
+		SIM->SCGC6 |= SPI1_CLOCK_GATING;
+		break;
+	case SPI_2:
+		SIM->SCGC3 |= SPI2_CLOCK_GATING;
+		break;
+	default:
+		break;
+	}
+}
+/*It configure the SPI as a master or slave depending on the value of masterOrslave*/
+static void SPI_setMaster(SPI_ChannelType channel, SPI_MasterType masterOrSlave){
+	switch(channel){
+	case SPI_0:
+		SPI0->MCR = (masterOrSlave<<31) & (1<<BIT31);
+		break;
+	case SPI_1:
+		SPI1->MCR = (masterOrSlave<<31) & (1<<BIT31);
+		break;
+	case SPI_2:
+		SPI2->MCR = (masterOrSlave<<31) & (1<<BIT31);
+		break;
+	default:
+		break;
+	}
+}
+/*It activate the TX and RX FIFOs of the SPI depending on the value of enableOrdisable*/
+static void SPI_fIFO(SPI_ChannelType channel, SPI_EnableFIFOType enableOrDisable){
+	switch(channel){
+	case SPI_0:
+		SPI0->MCR = (enableOrDisable<<13) & (1<<BIT13);
+		SPI0->MCR = (enableOrDisable<<12) & (1<<BIT12);
+		break;
+	case SPI_1:
+		SPI1->MCR = (enableOrDisable<<13) & (1<<BIT13);
+		SPI1->MCR = (enableOrDisable<<12) & (1<<BIT12);
+		break;
+	case SPI_2:
+		SPI2->MCR = (enableOrDisable<<13) | (1<<BIT13);
+		SPI2->MCR = (enableOrDisable<<12) | (1<<BIT12);
+		break;
+	default:
+		break;
+	}
+}
+/*It selects the clock polarity depending on the value of cpol*/
+static void SPI_clockPolarity(SPI_ChannelType channel, SPI_PolarityType cpol){
+	switch(channel){
+	case SPI_0:
+		SPI0->CTAR[26] |= (cpol<<26);
+		break;
+	case SPI_1:
+		SPI1->CTAR[26] |= (cpol<<26);
+		break;
+	case SPI_2:
+		SPI2->CTAR[26] |= (cpol<<26);
+		break;
+	default:
+		break;
+	}
+}
+/*It selects the frame size depending on the value of frameSize and the macros that are defined above*/
+static void SPI_frameSize(SPI_ChannelType channel, uint32 frameSize){
+	switch(channel){
+	case SPI_0:
+		SPI0->CTAR |= ;
+		break;
+	case SPI_1:
+		SPI1->CTAR |= ;
+		break;
+	case SPI_2:
+		SPI2->CTAR |= ;
+		break;
+	default:
+		break;
+	}
+}
+/*It selects the clock phase depending on the value of cpha*/
+static void SPI_clockPhase(SPI_ChannelType channel, SPI_PhaseType cpha){
+	switch(channel){
+	case SPI_0:
+		SPI0->CTAR[25] |= (cpha<<25);
+		break;
+	case SPI_1:
+		SPI1->CTAR[25] |= (cpha<<25);
+		break;
+	case SPI_2:
+		SPI2->CTAR[25] |= (cpha<<25);
+		break;
+	default:
+		break;
+	}
+}
+/*It selects the baud rate depending on the value of baudRate and the macros that are defined above*/
+static void SPI_baudRate(SPI_ChannelType channel, uint32 baudRate){
+
+	switch(channel){
+	case SPI_0:
+		SPI0->CTAR |= ;
+		break;
+	case SPI_1:
+		SPI1->CTAR |= ;
+		break;
+	case SPI_2:
+		SPI2->CTAR |= ;
+		break;
+	default:
+		break;
+	}
+}
+/*It selects if MSB or LSM bits is first transmitted*/
+static void SPI_mSBFirst(SPI_ChannelType channel, SPI_LSMorMSBType msb){
+	switch(channel){
+	case SPI_0:
+		SPI0->CTAR[24] |= (msb<<24);
+		break;
+	case SPI_1:
+		SPI1->CTAR[24] |= (msb<<24);
+		break;
+	case SPI_2:
+		SPI2->CTAR[24] |= (msb<<24);
+		break;
+	default:
+		break;
+	}
+}
+/*It stars the SPI transmission by modifying the value of HALT bit*/
+void SPI_startTranference(SPI_ChannelType channel){
+	switch(channel){
+	case SPI_0:
+		SPI0->MCR |= (0<<0);
+		break;
+	case SPI_1:
+		SPI1->MCR |= (0<<0);
+		break;
+	case SPI_2:
+		SPI2->MCR |= (0<<0);
+		break;
+	default:
+		break;
+	}
+}
+/*It stops the SPI transmission by modifying the value of HALT bit*/
+void SPI_stopTranference(SPI_ChannelType channel){
+	switch(channel){
+	case SPI_0:
+		SPI0->MCR |= (1<<0);
+		break;
+	case SPI_1:
+		SPI1->MCR |= (1<<0);
+		break;
+	case SPI_2:
+		SPI2->MCR |= (1<<0);
+		break;
+	default:
+		break;
+	}
+}
+/*It transmits the information contained in data*/
+void SPI_sendOneByte(uint8 Data){
+	SPI0->PUSHR = (Data);
+	while(FALSE == (SPI0->SR & SPI_SR_TCF_MASK));
+	SPI0->SR |= SPI_SR_TCF_MASK;
+
+}
+/*It configures the SPI for transmission, this function as arguments receives a pointer to a constant structure where are all
+ * the configuration parameters*/
+void SPI_init(const SPI_ConfigType* SPI_Config){
+
+	SPI_clk(SPI_Config->SPI_Channel);
+	GPIO_clockGating(SPI_Config->GPIOForSPI.GPIO_portName);
+	GPIO_pinControlRegister(SPI_Config->GPIOForSPI.GPIO_portName,SPI_Config->GPIOForSPI.SPI_clk,&(SPI_Config->pinConttrolRegisterPORTD));
+	GPIO_pinControlRegister(SPI_Config->GPIOForSPI.GPIO_portName,SPI_Config->GPIOForSPI.SPI_Sout,&(SPI_Config->pinConttrolRegisterPORTD));
+	SPI_enable(SPI_Config->SPI_Channel);
+	SPI_setMaster(SPI_Config->SPI_Channel, SPI_Config->SPI_Master);
+	SPI_fIFO(SPI_Config->SPI_Channel, SPI_Config->SPI_EnableFIFO);
+	SPI_clockPolarity(SPI_Config->SPI_Channel, SPI_Config->SPI_Polarity);
+	SPI_frameSize(SPI_Config->SPI_Channel, SPI_Config->frameSize);
+	SPI_clockPhase(SPI_Config->SPI_Channel, SPI_Config->SPI_Phase);
+	SPI_baudRate(SPI_Config->SPI_Channel, SPI_Config->baudrate);
+	SPI_mSBFirst(SPI_Config->SPI_Channel, SPI_Config->SPI_LSMorMSB);
+}
---
+++ main.c
@@ -0,0 +1,72 @@
+/**
+	\file 
+	\brief 
+		This is a starter file to use the Nokia 5510 LCD. 
+		The LCD is connected as follows:
+		reset-PDT0
+		CE-GND
+		DC-PTD3
+		DIN-PTD2
+		CLK-PTD1
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	1/08/2015
+	\todo
+	    The SPI device driver needs to be completed.
+ */
+
+ 
+//#include "SPI.h"
+#include "GPIO.h"
+#include "DatatypeDefinitions.h"
+#include "LCDNokia5110.h"
+#include "LCDNokia5110Images.h"
+#include "GlobalFunctions.h"
+#include "SPI.h"
+
+/** \brief This is the configuration structure to configure the LCD.
+ * Note that is constants and it is because is only a variable used for configuration*/
+const SPI_ConfigType SPI_Config={SPI_DISABLE_FIFO,
+							SPI_LOW_POLARITY,
+							SPI_LOW_PHASE,
+							SPI_MSB,
+							SPI_0,
+							SPI_MASTER,
+							GPIO_MUX2,
+							SPI_BAUD_RATE_2,
+							SPI_FSIZE_8,
+							{GPIOD,BIT1,BIT2}};
+/*! This array hold the initial picture that is shown in the LCD*/
+extern const uint8 ITESO[504];
+
+
+int main(void)
+{
+	uint8 string1[]="ITESO"; /*! String to be printed in the LCD*/
+	uint8 string2[]="uMs y DSPs"; /*! String to be printed in the LCD*/
+	SPI_init(&SPI_Config); /*! Configuration function for the LCD port*/
+	LCDNokia_init(); /*! Configuration function for the LCD */
+		for(;;) {	  
+			LCDNokia_clear();/*! It clears the information printed in the LCD*/
+			LCDNokia_bitmap(&ITESO[0]); /*! It prints an array that hold an image, in this case is the initial picture*/
+			delay(65000);
+			LCDNokia_clear();
+			delay(65000);
+     		LCDNokia_clear();
+     		LCDNokia_gotoXY(25,0); /*! It establishes the position to print the messages in the LCD*/
+			LCDNokia_sendString(string1); /*! It print a string stored in an array*/
+			delay(65000);
+     		LCDNokia_gotoXY(10,1);
+			LCDNokia_sendString(string2); /*! It print a string stored in an array*/
+			delay(65000);
+			LCDNokia_gotoXY(25,2);
+			LCDNokia_sendChar('2'); /*! It prints a character*/
+			LCDNokia_sendChar('0'); /*! It prints a character*/
+			LCDNokia_sendChar('1'); /*! It prints a character*/
+			LCDNokia_sendChar('5'); /*! It prints a character*/
+			delay(65000);
+
+		}
+	
+	return 0;
+}
+
