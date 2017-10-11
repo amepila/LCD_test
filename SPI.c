fdd315fdc8f5eac288136c24ba4f4a444f477810
amepila <ie704453@iteso.mx>
2017-10-11 11:43:53 -0500

    TCAR channel change
    All TCAR in channel 0

--- SPI.c
+++ SPI.c
@@ -100,12 +100,12 @@ static void SPI_clockPolarity(SPI_ChannelType channel, SPI_PolarityType cpol){//
 		else{SPI0->CTAR[0] |= SPI_CTAR_CPOL_MASK;}
 		break;
 	case SPI_1:
-		if(0 == cpol){SPI0->CTAR[1] &= ~(SPI_CTAR_CPOL_MASK);}
-		else{SPI0->CTAR[1] |= SPI_CTAR_CPOL_MASK;}
+		if(0 == cpol){SPI1->CTAR[0] &= ~(SPI_CTAR_CPOL_MASK);}
+		else{SPI1->CTAR[0] |= SPI_CTAR_CPOL_MASK;}
 		break;
 	case SPI_2:
-		if(0 == cpol){SPI0->CTAR[2] &= ~(SPI_CTAR_CPOL_MASK);}
-		else{SPI0->CTAR[2] |= SPI_CTAR_CPOL_MASK;}
+		if(0 == cpol){SPI2->CTAR[0] &= ~(SPI_CTAR_CPOL_MASK);}
+		else{SPI2->CTAR[0] |= SPI_CTAR_CPOL_MASK;}
 		break;
 	default:
 		break;
@@ -120,12 +120,12 @@ static void SPI_frameSize(SPI_ChannelType channel, uint32 frameSize){
 		SPI0->CTAR[0] |= frameSize;
 		break;
 	case SPI_1:
-		SPI1->CTAR[1] &= ~(SPI_CTAR_FMSZ_MASK);
-		SPI1->CTAR[1] |= frameSize;
+		SPI1->CTAR[0] &= ~(SPI_CTAR_FMSZ_MASK);
+		SPI1->CTAR[0] |= frameSize;
 		break;
 	case SPI_2:
-		SPI2->CTAR[2] &= ~(SPI_CTAR_FMSZ_MASK);
-		SPI2->CTAR[2] |= frameSize;
+		SPI2->CTAR[0] &= ~(SPI_CTAR_FMSZ_MASK);
+		SPI2->CTAR[0] |= frameSize;
 		break;
 	default:
 		break;
@@ -139,12 +139,12 @@ static void SPI_clockPhase(SPI_ChannelType channel, SPI_PhaseType cpha){//COMPLE
 		else{SPI0->CTAR[0] |= (SPI_CTAR_CPHA_MASK);}
 		break;
 	case SPI_1:
-		if(0 == cpha){SPI1->CTAR[1] &= ~(SPI_CTAR_CPHA_MASK);}
-		else{SPI1->CTAR[1] |= (SPI_CTAR_CPHA_MASK);}
+		if(0 == cpha){SPI1->CTAR[0] &= ~(SPI_CTAR_CPHA_MASK);}
+		else{SPI1->CTAR[0] |= (SPI_CTAR_CPHA_MASK);}
 		break;
 	case SPI_2:
-		if(0 == cpha){SPI2->CTAR[2] &= ~(SPI_CTAR_CPHA_MASK);}
-		else{SPI2->CTAR[2] |= (SPI_CTAR_CPHA_MASK);}
+		if(0 == cpha){SPI2->CTAR[0] &= ~(SPI_CTAR_CPHA_MASK);}
+		else{SPI2->CTAR[0] |= (SPI_CTAR_CPHA_MASK);}
 		break;
 	default:
 		break;
@@ -159,12 +159,12 @@ static void SPI_baudRate(SPI_ChannelType channel, uint32 baudRate){//COMPLETE
 		SPI0->CTAR[0] |= baudRate;
 		break;
 	case SPI_1:
-		SPI1->CTAR[1] &= ~(SPI_CTAR_BR_MASK);
-		SPI1->CTAR[1] |= baudRate;
+		SPI1->CTAR[0] &= ~(SPI_CTAR_BR_MASK);
+		SPI1->CTAR[0] |= baudRate;
 		break;
 	case SPI_2:
-		SPI2->CTAR[2] &= ~(SPI_CTAR_BR_MASK);
-		SPI2->CTAR[2] |= baudRate;
+		SPI2->CTAR[0] &= ~(SPI_CTAR_BR_MASK);
+		SPI2->CTAR[0] |= baudRate;
 		break;
 	default:
 		break;
@@ -178,12 +178,12 @@ static void SPI_mSBFirst(SPI_ChannelType channel, SPI_LSMorMSBType msb){//COMPLE
 		else{SPI0->CTAR[0] |= SPI_CTAR_LSBFE_MASK;}
 		break;
 	case SPI_1:
-		if(0 == msb){SPI1->CTAR[1] &= ~(SPI_CTAR_LSBFE_MASK);}
-		else{SPI1->CTAR[1] |= SPI_CTAR_LSBFE_MASK;}
+		if(0 == msb){SPI1->CTAR[0] &= ~(SPI_CTAR_LSBFE_MASK);}
+		else{SPI1->CTAR[0] |= SPI_CTAR_LSBFE_MASK;}
 		break;
 	case SPI_2:
-		if(0 == msb){SPI2->CTAR[2] &= ~(SPI_CTAR_LSBFE_MASK);}
-		else{SPI2->CTAR[2] |= SPI_CTAR_LSBFE_MASK;}
+		if(0 == msb){SPI2->CTAR[0] &= ~(SPI_CTAR_LSBFE_MASK);}
+		else{SPI2->CTAR[0] |= SPI_CTAR_LSBFE_MASK;}
 		break;
 	default:
 		break;
